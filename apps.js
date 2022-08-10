78816b121fdcf17a4221a7c6d9ab75d54126a98d
Esraa Anwar Atmeh <84777144+esraaaatmeh@users.noreply.github.com>
2022-08-11 00:08:03 +0300

    cc

--- apps.js
+++ apps.js
@@ -1,44 +1,27 @@
 'use strict';
 var form=document.getElementById('form')
-var table=document.getElementById('table')
+var submit = document.getElementById('submit');
+
 var f_id=1000;
+function g_f(counter){
+    ++f_id ;
+    return f_id
+};
+
 const allFood = [];
 function Food(f_id,foodName,type,price){
     this.f_id=f_id;
     this.foodName=foodName;
     this.type=type;
     this.price=price;
-    this.render();
     }
-
-Food.prototype.render = function () {
-
-        let r=document.createElement('tr')
-        table.appendChild(r)
-        
-        let f_id=document.createElement('td')
-        f_id.textContent=this.f_id;
-        r.appendChild(f_id)
-
-        let fName=document.createElement('td')
-        fName.textContent=this.foodName;
-        r.appendChild(fName)
-
-        let type=document.createElement('td')
-        type.textContent=this.type;
-        r.appendChild(type)
-
-        let price=document.createElement('td')
-        price.textContent=this.price;
-        r.appendChild(price)
-        r.classList.add('row');
+function saveData(data){
+    var stringData = JSON.stringify(allFood);
+    localStorage.setItem("Food",stringData);
 
     
-    }
-    function g_f(counter){
-        ++f_id ;
-        return f_id
-    };
+}
+    
 
 form.addEventListener("submit", handleSubmit);
 function handleSubmit(event) {
@@ -48,6 +31,8 @@ function handleSubmit(event) {
     let type = event.target.type.value;
     let price = event.target.price.value;    
     const newFood = new Food(f_id,foodName,type,price);
+    allFood.push(newFood);
+    saveData(allFood);
 
     
 }
